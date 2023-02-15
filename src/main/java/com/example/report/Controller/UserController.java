8b2279c3a954464d1672f53171c1cef33f91bf37
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-15 23:14:47 +0900

    feat: UserController
    @GetMapping
    -> @PostMapping

--- src/main/java/com/example/report/Controller/UserController.java
+++ src/main/java/com/example/report/Controller/UserController.java
@@ -4,9 +4,7 @@ import com.example.report.dto.LoginRequestDto;
 import com.example.report.dto.SignupRequestDto;
 import com.example.report.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -14,13 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-    @GetMapping("/signup")
+    @PostMapping("/signup")
     public String signupPage(SignupRequestDto signupRequestDto){
         userService.signup(signupRequestDto);
         return "회원 가입 완료";
     }
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public String loginPage(LoginRequestDto loginRequestDto){
         userService.login(loginRequestDto);
         return "로그인 완료";
