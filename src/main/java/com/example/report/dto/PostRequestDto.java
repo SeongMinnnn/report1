f68275cee1f8289f19fc013ca74ebd72e7cf7691
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-15 20:50:03 +0900

    refactor: PostRequestDto
    @Setter
    @NoArgsConstructor 추가
    게시물 작성시간 추가

--- src/main/java/com/example/report/dto/PostRequestDto.java
+++ src/main/java/com/example/report/dto/PostRequestDto.java
@@ -2,21 +2,26 @@ package com.example.report.dto;
 
 import com.example.report.entity.Post;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class PostRequestDto {
     private String username;
     private String contents;
     private String title;
     private String password;
+    private LocalDateTime createdAt;
 
     public PostRequestDto(Post post) {
         this.username = post.getUsername();
         this.contents = post.getContents();
         this.title = post.getTitle();
         this.password = post.getPassword();
-    }
-
-    public PostRequestDto(){
+        this.createdAt = post.getCreatedAt();
     }
 }
