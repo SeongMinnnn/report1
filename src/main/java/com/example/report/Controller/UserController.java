abc4295f8e7fb2ac9c7a6e9d3579e570c5f0a13d
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-15 23:03:42 +0900

    docs: UserController
    코드 정리

--- src/main/java/com/example/report/Controller/UserController.java
+++ src/main/java/com/example/report/Controller/UserController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.desktop.UserSessionEvent;
-
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
