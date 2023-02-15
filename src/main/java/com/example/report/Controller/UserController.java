f876b968ada0893adf798ba06a829142179fe9e1
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-15 22:31:15 +0900

    feat: UserController
    UserController 추가

--- src/main/java/com/example/report/Controller/UserController.java
+++ src/main/java/com/example/report/Controller/UserController.java
@@ -1,22 +1,28 @@
 package com.example.report.Controller;
 
+import com.example.report.dto.LoginRequestDto;
 import com.example.report.dto.SignupRequestDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.desktop.UserSessionEvent;
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+
+    private final UserService userService;
     @GetMapping("/signup")
-    public String signupPage(@RequestBody SignupRequestDto signupRequestDto){
+    public String signupPage(SignupRequestDto signupRequestDto){
         userService.signup(signupRequestDto);
         return "회원 가입 완료";
     }
 
     @GetMapping("/login")
-    public String loginPage(){
+    public String loginPage(LoginRequestDto loginRequestDto){
+        userService.login(loginRequestDto);
         return "로그인 완료";
     }
 }
