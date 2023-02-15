e8bf817b05542d7a82516e52aa323ec424ef4598
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-15 21:38:22 +0900

    feat: PostController
    @RequestMapping추가

--- src/main/java/com/example/report/postController/PostController.java
+++ src/main/java/com/example/report/postController/PostController.java
@@ -8,32 +8,33 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController // RESTAPI 컨트롤러 클래스에 사용, 해당 어노테이션이 적용된 메서드를 자동으로 실행한 결과를 HTTP 응답으로 반환
+@RequestMapping("/api")
+@RestController // Json 형태로 응답 반환
 @RequiredArgsConstructor // 클래스의 final 필드와 @NonNull 필드를 포함하는 생성자를 자동으로 생성
 public class PostController {
     private final PostService postService;
     //게시글 작성
-    @PostMapping("/api/post")
+    @PostMapping("/post")
     public PostResponseDto createPost(@RequestBody PostRequestDto requestDto){
         return postService.createPost(requestDto);
     }
 
-    @GetMapping("/api/search")
+    @GetMapping("/search")
     public List<PostResponseDto> getPosts() {
         return postService.getPosts();
     }
 
-    @GetMapping("/api/search/post")
+    @GetMapping("/search/post")
     public PostRequestDto getPost(@RequestBody Long id) {
         return postService.getPost(id);
     }
 
-    @PutMapping ("/api/post/{id}")
+    @PutMapping ("/post/{id}")
     public PostResponseDto updatePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto) {
         return postService.update(id, requestDto);
     }
 
-    @DeleteMapping("/api/post/{id}/delete")
+    @DeleteMapping("/post/{id}/delete")
     public String deletePost(@PathVariable Long id, @RequestBody PostRequestDto requestDto){
         return "삭제완료";
     }
