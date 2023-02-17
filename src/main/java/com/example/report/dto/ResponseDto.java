60775725fcff563c684eeaf436bfc2470aafab2c
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-18 00:42:37 +0900

    feat: ResponseDto
    String 값 int 로 변환하여 더 가볍게 함

--- src/main/java/com/example/report/dto/ResponseDto.java
+++ src/main/java/com/example/report/dto/ResponseDto.java
@@ -12,13 +12,13 @@ public class ResponseDto<T> {
     public static <T> ResponseDto<T> success(T data){
         return new ResponseDto<>(true, data, null);
     }
-    public static <T> ResponseDto<T> fail(String code, String message){
+    public static <T> ResponseDto<T> fail(int code, String message){
         return new ResponseDto<>(false, null, new Error(code, message));
     }
     @Getter
     @AllArgsConstructor
     static class Error{
-        private String code;
+        private int code;
         private String message;
     }
 }
