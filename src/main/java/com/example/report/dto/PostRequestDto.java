27691572b825858b77a07eca313380ba14983cd2
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-18 23:48:53 +0900

    feat: PostRequestDto
    불필요한 코드 제거

--- src/main/java/com/example/report/dto/PostRequestDto.java
+++ src/main/java/com/example/report/dto/PostRequestDto.java
@@ -9,13 +9,11 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 public class PostRequestDto {
-    private String username;
     private String contents;
     private String title;
     private LocalDateTime createdAt;
 
     public PostRequestDto(Post post) {
-        this.username = post.getUsername();
         this.contents = post.getContents();
         this.title = post.getTitle();
         this.createdAt = post.getCreatedAt();
