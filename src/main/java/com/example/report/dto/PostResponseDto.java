015223d0374d047eedb842dc8fcbe5f6fd3f8cf5
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-19 02:03:00 +0900

    feat: PostResponseDto
    필요 없는 코드 제거

--- src/main/java/com/example/report/dto/PostResponseDto.java
+++ src/main/java/com/example/report/dto/PostResponseDto.java
@@ -3,15 +3,12 @@ package com.example.report.dto;
 import com.example.report.entity.Post;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Setter
 @Getter
 @NoArgsConstructor
 public class PostResponseDto {
-    private String username;
     private String contents;
     private String title;
     private LocalDateTime createdAt;
