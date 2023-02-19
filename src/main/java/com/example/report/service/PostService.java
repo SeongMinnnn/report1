ab9afa35bd371785364921cc74531673659fa663
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-19 13:43:46 +0900

    dosc: Modified -> Created

--- src/main/java/com/example/report/service/PostService.java
+++ src/main/java/com/example/report/service/PostService.java
@@ -56,7 +56,7 @@ public class PostService {
 
     @Transactional(readOnly = true)
     public List<PostResponseDto> getPosts() {
-        List<Post> postList = postRepository.findAllByOrderByModifiedAtDesc();
+        List<Post> postList = postRepository.findAllByOrderByCreatedAtDesc();
         List<PostResponseDto> postResponseDtoList = new ArrayList<>();
         for (Post post : postList) {
             postResponseDtoList.add(new PostResponseDto(post));
