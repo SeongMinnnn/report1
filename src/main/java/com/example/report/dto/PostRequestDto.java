daef1aa217e1ed848fc5b5a0d909aad3569d38f3
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-19 20:33:14 +0900

    docs: PostRequestDto
    코드 정리

--- src/main/java/com/example/report/dto/PostRequestDto.java
+++ src/main/java/com/example/report/dto/PostRequestDto.java
@@ -1,10 +1,6 @@
 package com.example.report.dto;
 
-import com.example.report.entity.Post;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Getter
 public class PostRequestDto {
