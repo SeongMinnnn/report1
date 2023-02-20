3648f3ca3bc67c1c25e14a4ea1e835a27e8eea2f
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-21 02:22:57 +0900

    진유진 매니저님 빌더 설명

---
+++ src/main/java/com/example/report/dto/JinsDto.java
@@ -0,0 +1,29 @@
+//package com.example.report.dto;
+//
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.List;
+//
+//@Getter
+//@NoArgsConstructor
+//public class JinsDto {
+//
+//    private String title;
+//    private String content;
+//    private List<Comment> commentList;
+//
+//    @Getter
+//    @NoArgsConstructor
+//    public static class Comment{
+//        public String title;
+//        private String comment;
+//
+//        @Builder
+//        public Comment(String title, String comment) {
+//            this.title = title;
+//            this.comment = comment;
+//        }
+//    }
+//}
